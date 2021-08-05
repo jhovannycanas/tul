de6250c47811f7b2673d50257ec6f76e1eef18cb
Jhovanny <jhovannycanas@gmail.com>
2021-08-04 22:15:13 -0500

    Se crea estructura limpia proyecto, funcionalidades de producto y creación de carrito basico de compra

---
+++ ecommerce/src/main/java/com/tul/ecommerce/EcommerceTulApplication.java
@@ -0,0 +1,13 @@
+package com.tul.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceTulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceTulApplication.class, args);
+	}
+
+}
---
+++ ecommerce/src/test/java/com/tul/ecommerce/EcommerceTulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tul.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceTulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
